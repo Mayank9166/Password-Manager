95994aaf1ad1dffa6154dcede3208932228091ec
Mayank9166 <mayankapply9166@gmail.com>
2025-02-15 09:04:48 +0530

    add link to github

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
     <nav className='bg-slate-900 text-white'>
-        <div className="flex justify-between items-center px-4 py-5 h-14 mycontainer">
-        <div className="logo font-bold text-2xl ">
-            <span className='text-green-700'>&lt;</span>
-            Pass
-            <span className='text-green-700'>OP /&gt;</span>
-            </div>
+      <div className="flex justify-between items-center px-4 py-5 h-14 mycontainer">
+        <div className="logo font-bold text-2xl">
+          <span className='text-green-700'>&lt;</span>
+          Pass
+          <span className='text-green-700'>OP /&gt;</span>
+        </div>
 
-       {/* <ul >
-        <li className='flex gap-4' >
-            <a className='hover:font-bold text-xl' href="/">Home</a>
-            <a className='hover:font-bold text-xl' href="/">About</a>
-            <a className='hover:font-bold text-xl' href="/">Contact</a>
-        </li>
-       </ul> */}
-       <div className='flex justify-center items-center cursor-pointerring-current'>
-        <img className='rounded-full invert w-16 p-2 ' src="github.png" alt=""  />
-        <span className='font-bold'>Github</span>
-       </div>
-       </div>
-
+        <a 
+          href="https://github.com/Mayank9166" 
+          target="_blank" 
+         
+          className='flex justify-center items-center cursor-pointer'
+        >
+          <img className='rounded-full invert w-16 p-2' src="github.png" alt="GitHub" />
+          <span className='font-bold'>Github</span>
+        </a>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
